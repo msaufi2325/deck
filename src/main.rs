0a6328e357edc3a3817c9421ddd3b456766a8792
Saufi <msaufi2325@gmail.com>
2025-05-06 14:36:53 +0900

    Add initial Deck structure and main function implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug)]
+
+struct Deck {
+    cards: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // List of 'suit' - 'hearts', 'spades'
+    let suits: [&str; 2] = ["Hearts", "Spades"];
+
+    // List of 'values' 'ace', 'two', 'three'
+    let values: [&str; 3] = ["Ace", "Two", "Three"];
+
+    // Double nested for loop
+
+    let deck: Deck = Deck { cards: vec![] };
+
+    println!("Heres your deck: {:?}", deck);
 }
