bf1ec9bf3fe65d51446a33080f4f8ac5c24fac25
msaufi2325 <msaufi2325@gmail.com>
2025-05-17 03:58:09 +0900

    Implement Deck initialization and shuffling functionality

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
+use rand::{rng, seq::SliceRandom};
+
 #[derive(Debug)]
 
 struct Deck {
     cards: Vec<String>,
 }
 
+impl Deck {
+    fn new() -> Self {
+        // List of 'suit' - 'hearts', 'spades'
+        let suits: [&str; 2] = ["Hearts", "Spades"];
+
+        // List of 'values' 'ace', 'two', 'three'
+        let values: [&str; 3] = ["Ace", "Two", "Three"];
+
+        let mut cards: Vec<String> = vec![];
+
+        // Double nested for loop
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut t_rng = rng();
+        self.cards.shuffle(&mut t_rng);
+    }    
+}
+
 fn main() {
-    // List of 'suit' - 'hearts', 'spades'
-    let suits: [&str; 2] = ["Hearts", "Spades"];
+    let mut deck: Deck = Deck::new();
 
-    // List of 'values' 'ace', 'two', 'three'
-    let values: [&str; 3] = ["Ace", "Two", "Three"];
+    deck.shuffle();
 
-    // Double nested for loop
-
-    let deck: Deck = Deck { cards: vec![] };
-
-    println!("Heres your deck: {:?}", deck);
+    println!("Heres your deck: {:#?}", deck);
 }
